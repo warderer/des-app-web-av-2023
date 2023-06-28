e070cb458d6822b089b171c6a817f8381713e3fe
César Guerra <warderer@gmail.com>
2023-06-28 12:55:25 -0600

    ✨ Header con NavLink

--- 04-piquito-de-oro/src/components/Header.jsx
+++ 04-piquito-de-oro/src/components/Header.jsx
@@ -1,40 +1,42 @@
+import { NavLink } from 'react-router-dom'
+
 const Header = () => {
   return (
     <div className='container'>
       <header className='d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom'>
-        <a
-          href='/'
+        <NavLink
+          to='/'
           className='d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none'
         >
           <svg className='bi me-2' width={40} height={32}>
             <use xlinkHref='#bootstrap' />
           </svg>
           <span className='fs-4'>Piquito Dorado</span>
-        </a>
+        </NavLink>
         <ul className='nav nav-pills'>
 
           <li className='nav-item'>
-            <a href='/' className='nav-link active' aria-current='page'>
+            <NavLink to='/' className='nav-link' aria-current='page'>
               Inicio
-            </a>
+            </NavLink>
           </li>
 
           <li className='nav-item'>
-            <a href='/about' className='nav-link'>
+            <NavLink to='/about' className='nav-link'>
               Acerca de
-            </a>
+            </NavLink>
           </li>
 
           <li className='nav-item'>
-            <a href='/faq' className='nav-link'>
+            <NavLink to='/faq' className='nav-link'>
               Preguntas Frecuentes
-            </a>
+            </NavLink>
           </li>
 
           <li className='nav-item'>
-            <a href='/contact' className='nav-link'>
+            <NavLink to='/contact' className='nav-link'>
               Contacto
-            </a>
+            </NavLink>
           </li>
 
         </ul>
