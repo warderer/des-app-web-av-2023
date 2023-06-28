cb64856e21be373809a50a093e998a6fd8077875
César Guerra <warderer@gmail.com>
2023-06-28 11:01:08 -0600

    ✨ ProductCard Grayscale

--- 04-piquito-de-oro/src/components/ProductCard.jsx
+++ 04-piquito-de-oro/src/components/ProductCard.jsx
@@ -1,10 +1,12 @@
 const ProductCard = ({ id, title, description, url, price, quantity }) => {
+  const grayscale = quantity === 0 ? { filter: 'grayscale(100%) brightness(60%)' } : {}
+
   return (
     <div className='col-md-3 my-3 d-flex align-items-stretch'>
       <div className='card'>
         <a href={`/product/${id}`}>
           <div className='image-container'>
-            <img className='product-image' src={`/assets/img/${url}`} alt={title} />
+            <img className='product-image' src={`/assets/img/${url}`} alt={title} style={grayscale} />
           </div>
         </a>
         <div className='card-body d-flex flex-column justify-content-between'>
