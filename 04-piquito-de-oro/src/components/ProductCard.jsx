34e36c2f585ad982b55b726d87f63177f89cf98d
César Guerra <warderer@gmail.com>
2023-06-28 11:08:42 -0600

    ✨ Efecto Disponible y Agotado

--- 04-piquito-de-oro/src/components/ProductCard.jsx
+++ 04-piquito-de-oro/src/components/ProductCard.jsx
@@ -1,12 +1,17 @@
 const ProductCard = ({ id, title, description, url, price, quantity }) => {
   const grayscale = quantity === 0 ? { filter: 'grayscale(100%) brightness(60%)' } : {}
 
+  const availabilityText = quantity === 0 ? 'Agotado' : 'Disponible'
+
+  const availabilityClass = quantity === 0 ? 'availability-tag out-of-stock' : 'availability-tag'
+
   return (
     <div className='col-md-3 my-3 d-flex align-items-stretch'>
       <div className='card'>
         <a href={`/product/${id}`}>
           <div className='image-container'>
             <img className='product-image' src={`/assets/img/${url}`} alt={title} style={grayscale} />
+            <div className={availabilityClass}>{availabilityText}</div>
           </div>
         </a>
         <div className='card-body d-flex flex-column justify-content-between'>
