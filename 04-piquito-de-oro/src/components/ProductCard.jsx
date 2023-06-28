1d027efb95c7b477b8dcb45c142a80ecccffd049
César Guerra <warderer@gmail.com>
2023-06-28 10:38:36 -0600

    ✨ Product Card Basico

---
+++ 04-piquito-de-oro/src/components/ProductCard.jsx
@@ -0,0 +1,24 @@
+const ProductCard = ({ id, title, description, url, price, quantity }) => {
+  return (
+    <div className='col-md-3 my-3 d-flex align-items-stretch'>
+      <div className='card'>
+        <a href={`/product/${id}`}>
+          <div className='image-container'>
+            <img className='product-image' src={`/assets/img/${url}`} alt={title} />
+          </div>
+        </a>
+        <div className='card-body d-flex flex-column justify-content-between'>
+          <div>
+            <h5 className='card-title'>{title}</h5>
+            <p className='card-text'>{description}</p>
+          </div>
+
+          <div className='mx-auto mt-3'>
+            <p className='card-price'>${price}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default ProductCard
