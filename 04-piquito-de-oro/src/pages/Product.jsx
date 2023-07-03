5a61a871a31f27025c467e948eb27ee843081026
César Guerra <warderer@gmail.com>
2023-07-03 09:52:40 -0600

    ✨ useParams en Product

--- 04-piquito-de-oro/src/pages/Product.jsx
+++ 04-piquito-de-oro/src/pages/Product.jsx
@@ -1,6 +1,34 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
 const Product = () => {
+  const myRequest = new Request('../assets/lentes.json')
+  const [product, setProduct] = useState({})
+  const productId = useParams().id
+
+  useEffect(() => {
+    fetch(myRequest)
+      .then((response) => {
+        return response.json()
+      }).then((glasses) => {
+        const oneProduct = glasses.find((item) => item.id === productId)
+        setProduct(oneProduct)
+      }).catch((error) => {
+        console.error(error)
+      })
+  }, [])
+
+  if (!product) {
+    return <div>Error: Producto no encontrado</div>
+  }
+
   return (
-    <div>Product</div>
+    <div>
+      <p>El ID del Producto es {productId}</p>
+      <h2>Producto: {product.title}</h2>
+      <p>Descripcion: {product.description}</p>
+      <p>Precio: {product.price}</p>
+    </div>
   )
 }
 export default Product
