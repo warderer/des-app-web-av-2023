99f2d6d9e994178d3c86a6a72107be1bbf5cb0f8
César Guerra <warderer@gmail.com>
2023-07-03 11:19:31 -0600

    ✨ Cambio de prueba para Deploy

--- 04-piquito-de-oro/src/pages/About.jsx
+++ 04-piquito-de-oro/src/pages/About.jsx
@@ -1,6 +1,9 @@
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <h1>Piquito de Oro</h1>
+      <p>Tienda que vende accesorios a buen precio</p>
+    </div>
   )
 }
 export default About
